ec8ae1df1787b22dbeafe5e60d37ad2b71396f4b
Shawn Kelley <skelley@us.ci.org>
2022-05-17 12:25:58 -0600

    Add my user and test user

--- packages/stream_chat_v1/lib/app_config.dart
+++ packages/stream_chat_v1/lib/app_config.dart
@@ -1,8 +1,17 @@
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
-const kDefaultStreamApiKey = 'xw7atgefnpfh';
+const kDefaultStreamApiKey = 'tqdpjrr7us6k';
 
 final defaultUsers = <String, User>{
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoidGVzdF91c2VyIn0.2c6IoYEDK7iFJOi5cF5eWLIjws5WH9YxYOEeG2EbXIo':
+      User(
+    id: 'test_user',
+    extraData: {
+      'name': 'Test User',
+      'image':
+          'https://media-exp1.licdn.com/dms/image/C5603AQFsJtLMj7LegQ/profile-displayphoto-shrink_200_200/0/1548810018854?e=1658361600&v=beta&t=ivSVOd6Kmzl4MHRxML5grLPwfOWY1yVrT0P8HDzd7MI',
+    },
+  ),
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoic2tlbGxleSJ9.rCNQoUcATdYKLZ2R0GtL7QHKh1_lI_a7FwqAAAgy1_g':
       User(
     id: 'skelley',
